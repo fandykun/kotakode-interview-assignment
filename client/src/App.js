edec14d3f113712ba3c6cf465478cf3b26af79bb
fandykun <fandy19061999@gmail.com>
2021-01-23 14:11:42 +0700

    assignment 1 done

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
-import './App.css';
-import TodoList from './TodoList';
+import React, { useState } from "react";
+import "./App.css";
+import TodoList from "./TodoList";
 
 function App() {
-  const tasks = useState([
-    "Cuci Baju",
-    "Masak Nasi"
-  ]);
+  const [tasks, setTasks] = useState(["Cuci Baju", "Masak Nasi"]);
+
+  const [input, setInput] = useState("");
+
+  const handleInputTask = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleTambahTask = () => {
+    if (input !== "") setTasks(tasks.concat(input));
+    setInput("");
+  };
+
+  const handleHapusTask = (name) => {
+    setTasks(tasks.filter((task) => task !== name));
+  };
+
   return (
     <div className="App">
       <h1>Pekerjaan Rumah Yang Perlu Dilakukan</h1>
-      <input value=""/>
-      <button onClick="">Tambah</button>
-      <TodoList tasks={tasks} />
+      <input value={input} onChange={(event) => handleInputTask(event)} />
+      <button onClick={handleTambahTask}>Tambah</button>
+      <TodoList tasks={tasks} handleRemove={handleHapusTask} />
     </div>
   );
 }
--- client/src/TodoList.jsx
+++ client/src/TodoList.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-export default function TodoList() {
+export default function TodoList(props) {
+  const tasks = props.tasks;
+
   return (
     <div>
-      <li>Cuci Baju <button>Hapus</button></li>
-      <li>Masak Nasi <button>Hapus</button></li>
+      {tasks.map((task) => (
+        <li>
+          {task}
+          <button onClick={() => props.handleRemove(task)}>Hapus</button>
+        </li>
+      ))}
     </div>
   );
 }
